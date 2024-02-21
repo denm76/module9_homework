4175fe2a40fa56b36fea4e5ba431f9302881ea21
denm76 <denm76@gmail.com>
2024-02-21 20:29:11 +0200

    module9_homework:changed task 5

--- task9-5.js
+++ task9-5.js
@@ -7,13 +7,21 @@
 // Если число во втором input не попадает в диапазон от 1 до 10 или не является числом — выводить ниже текст «Лимит вне диапазона от 1 до 10»;
 // Если и первый, и второй input не в диапазонах или не являются числами — выводить ниже текст «Номер страницы и лимит вне диапазона от 1 до 10»;
 // Если числа попадают в диапазон от 1 до 10 — сделать запрос по URL https://jsonplaceholder.typicode.com/photos?_page=0&_limit=5, где GET-параметр _page — это число из первого input, а GET-параметр _limit — это введённое число второго input.
+// После получения данных вывести список картинок на экран.
+// Если пользователь перезагрузил страницу, то ему должны показываться картинки из последнего успешно выполненного запроса (использовать localStorage).
 
 const btn = document.querySelector("button");
 const resultNode = document.querySelector(".result");
 
+document.addEventListener("DOMContentLoaded", () => {
+  let storageItem = localStorage.getItem('lastResponse');
+  if (storageItem) {
+    displayResult(JSON.parse(storageItem));
+  }
+});
+
 function displayResult(apiData) {
   let cards = "";
-
   apiData.forEach(element => {
     const cardBlock = `
     <div class = "card">
@@ -25,28 +33,39 @@ function displayResult(apiData) {
   resultNode.innerHTML = cards;
 }
 
-btn.addEventListener('click', () => {
-  const firstInputValue = +document.getElementById("firstDigit").value;
-  const secondInputValue = +document.getElementById("secondDigit").value;
+const useRequest = async (page, limit) => {
+  return fetch(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      localStorage.setItem('lastResponse', JSON.stringify(data));
+      displayResult(data);
+    })
+    .catch((e) => {
+      console.log("error: " + e);
+    })
+}
 
-  if ((firstInputValue < 1 || firstInputValue > 10 || typeof (firstInputValue) !== 'number' || isNaN(firstInputValue)) && secondInputValue >= 1 && secondInputValue <= 10 && typeof (secondInputValue) === 'number' && !isNaN(secondInputValue)) {
-    resultNode.innerHTML = "Номер страницы вне диапазона от 1 до 10 input1";
-  } else if ((firstInputValue >= 1 && firstInputValue <= 10 && typeof (firstInputValue) === 'number' && !isNaN(firstInputValue)) && (secondInputValue < 1 || secondInputValue > 10 || typeof (secondInputValue) !== 'number' || isNaN(secondInputValue))) {
-    resultNode.innerHTML = "Номер страницы вне диапазона от 1 до 10 input2";
-  } else if (firstInputValue >= 1 && firstInputValue <= 10 && typeof (firstInputValue) === 'number' && !isNaN(firstInputValue) && secondInputValue >= 1 && secondInputValue <= 10 && typeof (secondInputValue) === 'number' && !isNaN(secondInputValue)) {
+btn.addEventListener('click', async () => {
+  const pageInputValue = +document.getElementById("firstDigit").value;
+  const limitInputValue = +document.getElementById("secondDigit").value;
 
-    fetch(`https://jsonplaceholder.typicode.com/photos?_page=${firstInputValue}&_limit=${secondInputValue}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        displayResult(data);
-      })
-      .catch((e) => {
-        console.log("error: " + e);
-      })
+  const firstInputMessageError = isNaN(pageInputValue) || pageInputValue < 1 || pageInputValue > 10;
+  const secondInputMessageError = isNaN(limitInputValue) || limitInputValue < 1 || limitInputValue > 10;
 
-  } else if (((firstInputValue < 1 || firstInputValue > 10 || typeof (firstInputValue) !== 'number' || isNaN(firstInputValue))) && (secondInputValue < 1 || secondInputValue > 10 || typeof (secondInputValue) !== 'number' || isNaN(secondInputValue))) {
+
+  if (firstInputMessageError) {
+    resultNode.innerHTML = "Номер страницы вне диапазона от 1 до 10";
+  }
+  if (secondInputMessageError) {
+    resultNode.innerHTML = "Лимит вне диапазона от 1 до 10";
+  }
+  if (!firstInputMessageError && !secondInputMessageError) {
+    const requestResult = await useRequest(pageInputValue, limitInputValue);
+    displayResult(requestResult);
+  }
+  if (firstInputMessageError && secondInputMessageError) {
     resultNode.innerHTML = "Номер страницы и лимит вне диапазона от 1 до 10";
   }
 })
